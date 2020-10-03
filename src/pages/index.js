45da69e28855dc0768775a3905173512464639c4
Angela Ivy <angelaivy1111@gmail.com>
2020-10-03 16:36:46 -0700

    updated typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ const IndexPage = (props) => (
       <p>Hi Everyone! Thank you so much for visiting our wedding website! We are very excited to celebrate our day with our closest family & friends.</p>
       <p>At this time, we are planning on moving ahead with the full wedding and as the date gets closer, we will be updating plans as needed for COVID restrictions.</p>
       <p>Our wedding will be held on private property in Wenas, WA. It is a semi-destination for most folks, so we are looking at different options for lodging and/or transportation as we have guests coming from all over. It's about a 45 minute drive NW from Yakima. Our Ceremony will be held on on a hill, overlooking the valley up to the Cascades, followed by a dinner reception. We have a poll and would love some feedback as we try to gauge what the needs and wants of our guests would be.</p>
-      <p>More details to come following the formal invitations.</p>
-      <p>Best,</p>
+      <p>More details to come following the formal invitations. Feel free to <a href="mailto: angelaivy1111@gmail.com">email us</a> if you have any questions.</p>
+      <p>Cheers!</p>
       <p>Angela & Terry</p>
     </section>
   </Layout>
