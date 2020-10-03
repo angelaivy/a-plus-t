ade38d39d6884723ab4b179dc76e7160910c047b
Angela Ivy <angelaivy1111@gmail.com>
2020-10-03 15:49:42 -0700

    added information to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-//import Image from "../components/image"
+import Image from "../components/image"
+
 
 const IndexPage = (props) => (
   <Layout>
   <SEO title="Home" />
-    
    <section className="hero">
       <h1>Angela Jean Ivy & Terry Jay Wilson</h1>
-      <p>June 12, 2021 ~ Wenas, WA</p>
-      <p>Check back on this website for more information as the date gets closer. We are planning the full wedding now and will change plans as needed with COVID.</p>
+      <p>June 12, 2021</p>
+      <p>Where the desert meets the trees ~ Wenas, WA</p>
+      <p>Hi Everyone! Thank you so much for visiting our wedding website! We are very excited to celebrate our day with our closest family & friends.</p>
+      <p>At this time, we are planning on moving ahead with the full wedding and as the date gets closer, we will be updating plans as needed for COVID restrictions.</p>
+      <p>Our wedding will be held on private property in Wenas, WA. It is a semi-destination for most folks, so we are looking at different options for lodging and/or transportation as we have guests coming from all over. It's about a 45 minute drive NW from Yakima. Our Ceremony will be held on on a hill, overlooking the valley up to the Cascades, followed by a dinner reception. We have a poll and would love some feedback as we try to gauge what the needs and wants of our guests would be.</p>
+      <p>More details to come following the formal invitations.</p>
+      <p>Best,</p>
+      <p>Angela & Terry</p>
     </section>
   </Layout>
 )
