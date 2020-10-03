8875a7f90c630e32c3ceecf4d38139edcf221571
Angela Ivy <angelaivy1111@gmail.com>
2020-10-03 15:22:25 -0700

    removed image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Image from "../components/image"
+//import Image from "../components/image"
 
 const IndexPage = (props) => (
   <Layout>
@@ -10,7 +10,6 @@ const IndexPage = (props) => (
    <section className="hero">
       <h1>Angela Jean Ivy & Terry Jay Wilson</h1>
       <p>June 12, 2021 ~ Wenas, WA</p>
-      <Image />
       <p>Check back on this website for more information as the date gets closer. We are planning the full wedding now and will change plans as needed with COVID.</p>
     </section>
   </Layout>
