ac9d288ccee6883ed61138cfbac4663990cb435c
Angela Ivy <angelaivy1111@gmail.com>
2020-10-04 12:49:44 -0700

    added nbsp to date

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => (
   <footer>
     <div className="content">
-    <p>Angela Ivy&nbsp;&nbsp;+&nbsp;&nbsp;Terry Wilson&nbsp;&nbsp;&infin;&nbsp;&nbsp;June 12, 2021</p>
+    <p>Angela Ivy&nbsp;&nbsp;+&nbsp;&nbsp;Terry Wilson&nbsp;&nbsp;&infin;&nbsp;&nbsp;June&nbsp;12,&nbsp;2021</p>
     <p><a href="mailto:angelaivy1111@gmail.com">Email Angela</a>&nbsp;&nbsp;&infin;&nbsp;&nbsp;<a href="mailto:t.dubo5o@gmail.com">Email Terry</a></p>
     </div>
   </footer>
