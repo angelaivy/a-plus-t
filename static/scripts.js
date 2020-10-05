f7d4fc7e21d8987f0e2d0cf20d32a9b2fa597876
Angela Ivy <angelaivy1111@gmail.com>
2020-10-04 18:03:49 -0700

    removed content in script for now

--- static/scripts.js
+++ static/scripts.js
@@ -1,20 +1,20 @@
-// Method #1: Detect IE 10 and IE 11
-function isIE() {
-  // IE 10 and IE 11
-  return /Trident\/|MSIE/.test(window.navigator.userAgent);
-}
+// // Method #1: Detect IE 10 and IE 11
+// function isIE() {
+//   // IE 10 and IE 11
+//   return /Trident\/|MSIE/.test(window.navigator.userAgent);
+// }
 
-let showBrowserAlert = (function () {
-  if (document.querySelector('.unsupported-browser')) {
-      let d = document.getElementsByClassName('unsupported-browser');
+// let showBrowserAlert = (function () {
+//   if (document.querySelector('.unsupported-browser')) {
+//       let d = document.getElementsByClassName('unsupported-browser');
 
-      is_IE11 = isIE11();
+//       is_IE11 = isIE11();
 
-      if (is_IE11) {
-          d[0].innerHTML = '<b>Unsupported Browser!</b> This website will offer limited functionality in this browser. IE is no longer supported, please visit this website on a more recent version of major browsers like Chrome, Firefox, Safari, and Edge.';
-          d[0].style.display = 'block';
-      }
-  }
-});
+//       if (is_IE11) {
+//           d[0].innerHTML = '<b>Unsupported Browser!</b> This website will offer limited functionality in this browser. IE is no longer supported, please visit this website on a more recent version of major browsers like Chrome, Firefox, Safari, and Edge.';
+//           d[0].style.display = 'block';
+//       }
+//   }
+// });
 
-document.addEventListener('DOMContentLoaded', showBrowserAlert);
+// document.addEventListener('DOMContentLoaded', showBrowserAlert);
