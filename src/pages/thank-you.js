0df662b9d2563742df3d46c71e99e1048507fa23
Angela Ivy <angelaivy1111@gmail.com>
2021-01-09 10:02:51 -0800

    added thank you page for testing form

---
+++ src/pages/thank-you.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThankYouPage = () => (
+  <Layout>
+    <SEO title="Thank you!" />
+    <h1>Thank you!</h1>
+    <p>Testing thank you</p>
+  </Layout>
+)
+
+export default ThankYouPage
