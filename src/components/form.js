be9ff37950bf5a409f4031c9a65f638980818fa8
Angela Ivy <angelaivy1111@gmail.com>
2021-01-08 22:38:13 -0800

    pushing changes

--- src/components/form.js
+++ src/components/form.js
@@ -9,26 +9,26 @@ const Form = () => (
       <input id="name" type="text" required></input>
 
       <p>Will you be able to attend?</p>
-      <label for="attending">Attending</label>
+      <label htmlFor="attending">Attending</label>
       <input type="radio" id="attending" name="attending"></input>
 
-      <label for="not-attending">Not Attending</label>
+      <label htmlFor="not-attending">Not Attending</label>
       <input type="radio" id="not-attending" name="attending"></input>
 
-      <label for="how-many">Number in party</label>
+      <label htmlFor="how-many">Number in party</label>
       <input type="number" id="how-many" name="how-many" min="0" max="8"></input>
 
       <p>Will you need accomodations?</p>
-      <label for="hotel">Hotel</label>
+      <label htmlFor="hotel">Hotel</label>
       <input type="radio" id="hotel" name="accomodations"></input>
 
-      <label for="camping">On Site Camping</label>
+      <label htmlFor="camping">On Site Camping</label>
       <input type="radio" id="camping" name="accomodations"></input>
 
-      <label for="no-accom">No accomodations needed</label>
+      <label htmlFor="no-accom">No accomodations needed</label>
       <input type="radio" id="no-accom" name="accomodations"></input>
       
-      <label for="song">What song would bring you out on the dance floor?</label>
+      <label htmlFor="song">What song would bring you out on the dance floor?</label>
       <input id="song" type="text"></input>
 
       <button type="submit">Submit</button>
